7a43bf9b2488ad0bc35cbd5c3d433ff8b3d0f958
Rajesh Kumar <mac@macs-MacBook-Pro.local>
2024-10-09 01:49:32 +0500

    add user login

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ApiLoginRequest;
+use App\Models\User;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,6 +18,13 @@ class AuthController extends Controller
         if(!Auth::attempt($request->only('email', 'password'))) {
             return $this->error('Invalid Credentials', 401);
         }
+        $user = User::firstWhere('email', $request->email);
+        return $this->ok(
+            'Authenticated',
+            [
+                'token' => $user->createToken('Api token for'.$user->email)->plainTextToken
+            ]
+            );
     }
 
     public function register()
--- app/Traits/ApiResponses.php
+++ app/Traits/ApiResponses.php
@@ -3,14 +3,15 @@
 namespace App\Traits;
 
 trait ApiResponses {
-    protected function ok($message) {
-        return $this->success($message, 200);
+    protected function ok($message, $data) {
+        return $this->success($message,$data, 200);
     }
 
-    protected function success($message, $statusCode = 200) {
+    protected function success($message,$data, $statusCode = 200) {
         return response()->json([
             'message' => $message,
-            'status' => $statusCode
+            'status' => $statusCode,
+            'data' => $data
         ], $statusCode);
     }
 
