7c44e6bd8aafcc47a2491aa99bbe5f0cd17c9eb7
Rajesh Kumar <mac@macs-MacBook-Pro.local>
2024-10-09 00:26:58 +0500

    api trait

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
