a14acb6caea85354e04167dab66f0c020df2cdc5
Rajesh Kumar <mac@macs-MacBook-Pro.local>
2024-10-10 00:48:08 +0500

    ticket filter

---
+++ app/Http/Filters/V1/TicketFilter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Filters\V1;
+
+class TicketFilter extends QueryFilter
+{
+    public function status($value)
+    {
+        return $this->builder->where('status', $value);
+    }
+}
